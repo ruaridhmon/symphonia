894129554aab07a122c83c7fcda3e64f8ac097f2
Hephaestus <hephaestus@antreas.io>
2026-02-24 00:27:07 +0000

    fix: replace BarChart3 with ChartNoAxesColumn for lucide-react v0.575

---
+++ frontend/dist/assets/forms-3ec29a03.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-21b2a623.js";function n(){return o.get("/forms")}function e(r){return o.get(`/forms/${r}`)}function s(){return o.get("/my_forms")}function m(r){return o.post("/forms/unlock",{join_code:r})}export{e as a,n as b,s as g,m as u};
